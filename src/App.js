45cba60b6ce4d02cf0050538753211a5c6a19655
Zain Shakir <shakir.zain@yahoo.com>
2023-09-15 14:14:35 +0500

    ACD

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-
-import './App.css';
-import { HashRouter as Router, Route, Routes} from 'react-router-dom';
-import { AuthProvider } from './Context/AuthContext';
-import Login from './Pages/Login';
-import PrivateRoute from './Routes/PrivateRoute';
-import ResetPassword from './Pages/ResetPassword';
-import Dashboard from './Pages/Dashboard';
-import UserType from './Pages/UserType';
+import "./App.css";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./Context/AuthContext";
+import Login from "./Pages/Login";
+import PrivateRoute from "./Routes/PrivateRoute";
+import ResetPassword from "./Pages/ResetPassword";
+import Dashboard from "./Pages/Dashboard";
+import UserType from "./Pages/UserType";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <Routes>
-        <Route path="/"
-                element={
-                  <PrivateRoute>
-                    <Dashboard/>
-                  </PrivateRoute>
-                }
-              ></Route>
-          <Route path='/Usertype' element={<UserType/>}/>
-          <Route path='/Login' element={<Login/>}/>
-          <Route path='/ResetPassword' element={<ResetPassword/>}/>
-         
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route path="/Usertype" element={<UserType />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/ResetPassword" element={<ResetPassword />} />
         </Routes>
       </AuthProvider>
     </Router>
